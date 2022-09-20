ec1184b89a2fd388affc95a95487c305615b8ced
masontmorris <masonmorris8@gmail.com>
2022-09-20 13:52:28 -0400

    adds hover effect to project screenshots

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,6 +9,9 @@ class Project extends React.Component {
                     <img src={this.props.image} alt={this.props.title} />
                 </a>
                 <h1>{this.props.title}</h1>
+                <a href={this.props.repoLink} target="_blank">
+                    Github repository
+                </a>
                 <p>{this.props.description}</p>
             </Card>
         );
--- src/main.jsx
+++ src/main.jsx
@@ -12,18 +12,21 @@ ReactDOM.createRoot(document.getElementById("root")).render(
                 title="Fan Food"
                 description="A simple web app which finds restaurants and bars near upcoming live events. Made using vanilla Javascript, JQuery, the TicketMaster Discover and Foursquare Places APIs, and Bulma for styling."
                 link="https://masontmorris.github.io/fanfood/"
+                repoLink="https://github.com/masontmorris/fanfood"
                 image="./src/assets/fanfood-screenshot.png"
             />
             <Project
                 title="HONGRY"
                 description="A web app where users can create and login to accounts to upload and view recipes. Made using Node, Express, Handlebars, MySQL, Sequelize, Cloudinary, Multer, Bcrypt, and Bootstrap for styling."
                 link="https://hongry.herokuapp.com/"
+                repoLink="https://github.com/Emmanuel6093/hongry"
                 image="./src/assets/hongry-screenshot.png"
             />
             <Project
                 title="Sapient"
                 description="A full stack web app where users can organize or volunteer for local community events. Made using React, Node, MongoDB, Mongoose, Express, GraphQL, and Apollo Server."
                 link="https://sapient-origin.herokuapp.com/"
+                repoLink="https://github.com/PrimitiveJ/Sapient-Community-Outreach-Application"
                 image="./src/assets/sapient-screenshot.png"
             />
         </div>
