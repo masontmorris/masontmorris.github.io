fabb1d48ced0612c2e67a8e877658ec986fd097f
masontmorris <masonmorris8@gmail.com>
2022-09-28 01:12:50 -0400

    began styling contact form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 
 const FORM_ENDPOINT = "https://public.herotofu.com/v1/e7fe2340-3edd-11ed-a10f-d1a38bd15d37";
 
@@ -24,19 +25,21 @@ const contactForm = () => {
 
     return (
         <Container id="contact">
-            <form action={FORM_ENDPOINT} method="POST" target="_blank">
-                <input type="text" className="form-input" id="form-name" placeholder="Your name" name="name" value={name} onChange={handleChange} />
-                <input type="email" className="form-input" id="form-email" placeholder="Your email" name="email" value={email} onChange={handleChange} />
-                <label>
-                    Are you reaching out on behalf of a company?
-                    <input type="checkbox" className="form-checkbox" name="companyCheckbox" checked={companyCheckbox} onChange={handleChange} />
-                </label>
-                {companyCheckbox && <input type="text" className="form-input" id="form-company" placeholder="Your company" name="company" value={company} onChange={handleChange} />}
-                <textarea className="form-input" id="form-message" placeholder="Your message" name="message" value={message} onChange={handleChange} />
-                <button type="submit" className="form-button" id="form-submit">
+            <Form action={FORM_ENDPOINT} method="POST" target="_blank" id="contact-form">
+                <input type="text" className="form-input form-control" id="form-name" placeholder="Your name" name="name" value={name} onChange={handleChange} />
+                <input type="email" className="form-input form-control" id="form-email" placeholder="Your email" name="email" value={email} onChange={handleChange} />
+                <div style={{ display: "flex" }}>
+                    <input type="checkbox" className="form-checkbox form-check" name="companyCheckbox" checked={companyCheckbox} onChange={handleChange} />
+                    <label htmlFor="companyCheckbox" className="form-check-label">
+                        Are you reaching out on behalf of a company?
+                    </label>
+                </div>
+                {companyCheckbox && <input type="text" className="form-input form-control" id="form-company" placeholder="Your company" name="company" value={company} onChange={handleChange} />}
+                <textarea className="form-input form-control" id="form-message" placeholder="Your message" name="message" value={message} onChange={handleChange} />
+                <button type="submit" className="form-button btn btn-dark" id="form-submit">
                     Submit
                 </button>
-            </form>
+            </Form>
         </Container>
     );
 };
