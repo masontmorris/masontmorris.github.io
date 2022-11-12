cc4bbf05221336218339a7908689937602ae0f16
masontmorris <masonmorris8@gmail.com>
2022-11-12 02:24:08 -0500

    please im begging

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import ProjectsQuery from "./components/Projects-Query";
-import Contact from "./components/ContactForm";
+import ContactForm from "./components/ContactForm";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -13,6 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Hero />
         <About />
         <ProjectsQuery />
-        <Contact />
+        <ContactForm />
     </React.StrictMode>
 );
